281ca06f30288a4bbdc5252497be542e31a7d128
Кириченко Александр <KiAS@gendalf.ru>
2021-03-16 12:49:22 +0300

    update description

--- example/index.php
+++ example/index.php
@@ -2,30 +2,23 @@
 
 include "../src/bootstrap.php";
 
-$token_str = "any_token_string";
-$date_create_token = \DateTime();
+$token_str = "строка с токеном";
 
-$token = Token($token_str, $date_create_token)
+$token = Token($token_str, $date_create_token);
 
-$settings = new \InstagramAPI\Settings($token);
+//update token
+// $token->update();
 
-$settings
-	//разрешаем обновление
-    ->setCanUpdateToken(true)
-    //устанавливает функцию которая вызовется после обновления токена (чтобы сохранить)
-    ->setCallbackUpdateToken(function(string $token, \DateTime $date){
-    	//сериализуем чтобы сохранить в БД или файл
-        $token_serial = serialize(new \InstagramAPI\Token\Token($token, $date));
-        //...
-    })
-    //определяем дату вкогда нужно обновить токен (к дате создания прибавляем 30 дней)
-    ->setDateToUpdateToken((clone $this->token->getDateCreate())->modify("+30 days"))
-;
+// Токен сериализируется сохраняя сам токен и время окончания его действия (+50 дней с начала обновления или создания)
+// $data = serialize($token);
+// $token = unserialize($data);
 
-//инициализируем клиента
-$instaClient = new \InstagramAPI\Client($settings);
+\BasicInstagram\Request::getInstance()->setToken($token->getToken());
 
-$instaClient->getMedia(); //медиа пользователя
-$instaClient->getUserData() //данные пользователя
+$me = new \BasicInstagram\Me();
 
+$me->getMedia()->getNext(12); //медиа пользователя с лимитом в 12
+//lazy load
+$me->getUsername(); //логин пользователя
+$me->getID(); //id пользователя
 ?>
